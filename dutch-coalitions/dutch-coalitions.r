ef596dcc1ed7ebd2c856bb5ce9712a948007b677
Rob Hanssen <rob.hanssen@outlook.com>
2023-07-11 14:43:54 -0400

    streamlining order of operations

--- dutch-coalitions/dutch-coalitions.r
+++ dutch-coalitions/dutch-coalitions.r
@@ -38,22 +38,93 @@ cab_data <-
             "\\(.*\\)|\\[\\d{1}\\]"
         )),
         across(aantreden:aftreden, dmy),
-        partijlist = as.list(strsplit(partijen, ", ")),
+        partijlist = strsplit(partijen, ", "),
         min_pres_partij = str_remove_all(minister_president, ".*\\(|\\)"),
     ) %>%
     replace_na(list(aftreden = ymd(20230707))) %>%
     mutate(cab_length = (aftreden - aantreden) / dyears(1))
-
-
 
 in_cab_leng <-
     cab_data %>%
     unnest_longer(partijlist) %>%
     group_by(partijlist) %>%
     summarize(in_cab = sum(cab_length)) %>%
-    rename(partij = partijlist, in_kabinet = in_cab) %>%
-    mutate(in_kabinet = round(in_kabinet, 1)) %>%
-    arrange(-in_kabinet)
+    arrange(-in_cab)
+
+colors <-
+    c(
+        "KVP" = "darkgreen",
+        "CHU" = "darkgreen",
+        "CDA" = "darkgreen",
+        "ARP" = "darkgreen",
+        "VDB" = "red",
+        "PvdA" = "red",
+        "DS'70" = "red",
+        "VVD" = "blue",
+        "D66" = "orange",
+        "PPR" = "lightgreen",
+        "LPF" = "black",
+        "ChristenUnie" = "lightblue"
+    )
+
+min_pres_alpha <-
+    c(
+        "TRUE" = 1,
+        "FALSE" = .3
+    )
+
+p1 <-
+    cab_data %>%
+    unnest_longer(partijlist) %>%
+    semi_join(in_cab_leng %>% filter(in_cab > 5),
+        by = c("partijlist")
+    ) %>%
+    mutate(min_pres = partijlist == min_pres_partij) %>%
+    ggplot() +
+    aes(y = factor(partijlist,
+        ordered = TRUE,
+        levels = rev(in_cab_leng$partijlist)
+    )) +
+    geom_segment(
+        aes(
+            x = aantreden, xend = aftreden,
+            yend = partijlist, color = partijlist,
+            alpha = min_pres
+        ),
+        linewidth = 8
+    ) +
+    scale_color_manual(values = colors) +
+    scale_alpha_manual(values = min_pres_alpha) +
+    labs(
+        x = "", y = "",
+        caption = glue::glue(
+            "Partijen met coalitiedeelname meer dan 5 jaar sinds 1945.\n",
+            "Transparante kleuren tonen kabinetsdeelname, ",
+            "niet-transparante kleur toont partij van de premier."
+        )
+    )
+
+ggsave("dutch-coalitions/party-in-gov.png",
+    height = 3, width = 7,
+    plot = p1
+)
+
+p2 <-
+    cab_data %>%
+    group_by(minister_president) %>%
+    mutate(eerst_cab = first(aantreden)) %>%
+    ungroup() %>%
+    ggplot(aes(y = fct_reorder(minister_president, desc(eerst_cab)))) +
+    geom_segment(
+        aes(
+            x = aantreden, xend = aftreden,
+            yend = minister_president, color = min_pres_partij
+        ),
+        linewidth = 8, alpha = 1
+    ) +
+    scale_color_manual(values = colors) +
+    labs(x = "", y = "")
+
 
 cab_data %>%
     filter(aantreden > ymd(19750101)) %>%
@@ -70,70 +141,4 @@ cab_data %>%
     mutate(min_pres_tijd = round(min_pres_tijd, 1)) %>%
     knitr::kable(caption = "Partij van de minister-president sinds 1975")
 
-
-colors <-
-    c(
-        "KVP" = "darkgreen",
-        "CHU" = "darkgreen",
-        "CDA" = "darkgreen",
-        "ARP" = "darkgreen",
-        "VDB" = "red",
-        "PvdA" = "red",
-        "VVD" = "blue",
-        "D66" = "orange",
-        "DS'70" = "red",
-        "PPR" = "lightgreen",
-        "LPF" = "black",
-        "ChristenUnie" = "lightblue"
-    )
-
-min_pres_alpha <-
-    c("TRUE" = 1, 
-    "FALSE" = .3)
-
-
-cab_data %>%
-    group_by(minister_president) %>%
-    mutate(eerst_cab = first(aantreden)) %>%
-    ungroup() %>%
-    ggplot(aes(y = fct_reorder(minister_president, desc(eerst_cab)))) +
-    geom_segment(
-        aes(
-            x = aantreden, xend = aftreden,
-            yend = minister_president, color = min_pres_partij
-        ),
-        linewidth = 8, alpha = 1
-    ) +
-    scale_color_manual(values = colors) +
-    labs(x = "", y = "")
-
-cab_data %>%
-    unnest_longer(partijlist) %>%
-    semi_join(in_cab_leng %>% filter(in_kabinet > 5),
-        by = c("partijlist" = "partij")
-    ) %>%
-    mutate(min_pres = partijlist == min_pres_partij) %>%
-    ggplot() +
-    aes(y = factor(partijlist,
-        ordered = TRUE,
-        levels = rev(in_cab_leng$partij)
-    )) +
-    geom_segment(
-        aes(
-            x = aantreden, xend = aftreden,
-            yend = partijlist, color = partijlist,
-            alpha = min_pres
-        ),
-        linewidth = 8
-    ) +
-    scale_color_manual(values = colors) +
-    scale_alpha_manual(values = min_pres_alpha) +
-    labs(x = "", y = "",
-        caption = glue::glue("Partijen met coalitiedeelname meer dan 5 jaar sinds 1945.\n",
-        "Transparante kleuren tonen kabinetsdeelname, niet-transparante kleur toont partij van de premier."))
-
-ggsave("dutch-coalitions/party-in-gov.png", height = 3, width = 7)
-
 Sys.setlocale("LC_TIME", oldloc)
-
-
