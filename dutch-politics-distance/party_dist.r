35c8a053e34bbad44e045b9f335a082c11d4a4c9
Rob Hanssen <rob.hanssen@outlook.com>
2025-10-24 14:45:39 -0400

    updates

--- dutch-politics-distance/party_dist.r
+++ dutch-politics-distance/party_dist.r
@@ -20,35 +20,6 @@ nl_parties <-
 
 lvls <- nl_parties$party
 
-# nl_parties %>%
-#     filter(party == p1 | party == p2) %>%
-#     select(-(1:7), -(9:12), -14) %>%
-#     t(.) %>%
-#     as_tibble() %>%
-#     set_names(c("a", "b")) %>%
-#     slice(-1) %>%
-#     mutate(across(everything(), as.numeric)) %>%
-#     mutate(dist = euclid(a, b)) %>%
-#     reframe(x = sum(dist, na.rm = TRUE))
-
-
-
-ccs <- combn(nl_parties$party, 2, function(x) {
-    party1 <- nl_parties %>% filter(party == x[1])
-    party2 <- nl_parties %>% filter(party == x[2])
-
-    galtan_dist <- euclid(party1$galtan, party2$galtan)
-    immigrate_dist <- euclid(party1$immigrate_policy, party2$immigrate_policy)
-    total_dist <- galtan_dist + immigrate_dist
-
-    list(
-        party1 = x[1],
-        party2 = x[2],
-        total_dist = total_dist
-    )
-})
-
-
 ccs <- combn(nl_parties$party, 2, function(x) {
     party1 <- nl_parties %>% filter(party == x[1])
     party2 <- nl_parties %>% filter(party == x[2])
@@ -63,8 +34,6 @@ ccs <- combn(nl_parties$party, 2, function(x) {
         mutate(across(everything(), as.numeric)) %>%
         mutate(dist = euclid(a, b)) %>%
         reframe(x = sum(dist, na.rm = TRUE))
-
-
 
     list(
         party1 = x[1],
@@ -83,7 +52,7 @@ dist_df <-
     ) %>%
     mutate(
         distance = as.numeric(distance),
-        dist_measure = scale(distance),
+        dist_measure = distance / max(distance),
         party1 = factor(party1, levels = lvls),
         party2 = factor(party2, levels = lvls)
     )
@@ -91,14 +60,13 @@ dist_df <-
 dist_df %>%
     ggplot(aes(x = party1, y = party2, fill = dist_measure)) +
     geom_tile() +
-    # scale_fill_gradient2(low = "green", mid = "white", high = "red") +
     scale_fill_distiller(palette = "Spectral", direction = -1) +
     labs(
         x = NULL, y = NULL, fill = "Ideologische\nafstand",
         caption = "Source: https://www.chesdata.eu/ches-europe"
     ) +
     coord_equal() +
-    theme_minimal() +
+    theme_light() +
     theme(
         legend.position = "inside",
         legend.position.inside = c(.9, .2),
@@ -109,5 +77,5 @@ dist_df %>%
 
 ggsave("dutch-politics-distance/party_distance_heatmap.png",
     width = 6,
-    height = 5
+    height = 6
 )
